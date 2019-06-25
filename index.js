0b1df9c009bf3ad40476cf52a59d0d63e0f798d8
Catalin <ucshadow@users.noreply.github.com>
2019-06-25 14:38:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const http = require('http');
+const path = require('path');
+const filePath = path.join(__dirname, 'data.dt');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    r = '';
-    const testFolder = './';
+    r = 'and the data is: ';
     const fs = require('fs');
 
-    fs.readdir(testFolder, (err, files) => {
-      files.forEach(file => {
-        r += file;
-      });
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+        if (!err) {
+            r += data
+        } else {
+            r += (err);
+        }
+        response.end(r);
     });
-    response.end(r);
+    
+    
 });
 
 const port = process.env.PORT || 1098;

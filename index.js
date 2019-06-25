ab0093f9b553e1263a7c0b8496217b4587a4e40f
Catalin <ucshadow@users.noreply.github.com>
2019-06-25 13:14:08 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Momo");
+});
+
+const port = process.env.PORT || 1098;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);

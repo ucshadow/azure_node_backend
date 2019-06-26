18ea9ede7eb0520e98c1d8fdbdf2b8e6af0b4362
catud <catalin.udrea@systematic.com>
2019-06-26 08:33:34 +0200

    added git ignore

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-const http = require('http');
-const path = require('path');
-const filePath = path.join(__dirname, 'data.dt');
+const express = require("express");
+const app = express();
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    r = 'and the data is: ';
-    const fs = require('fs');
 
-    fs.readFile('/home/data.dt', {encoding: 'utf-8'}, function(err,data){
-        if (!err) {
-            r += data
-        } else {
-            r += (err);
-        }
-        response.end(r);
-    });
-    
-    
+app.get("/", (req, res, next) => {
+    res.json(["Momo","Lisa","Michael","Ginger","Food"]);
 });
 
-const port = process.env.PORT || 1098;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(1098, () => {
+    console.log("Server running on port 3000");
+});
